dc1968ce26261c185ff7aebee271f22f5b049fc8
Ricardo Maes <ricmzn@gmail.com>
2021-03-29 02:06:19 -0300

    Add terrain streaming feature

--- src/windows/terrain_window.rs
+++ src/windows/terrain_window.rs
@@ -1,4 +1,15 @@
-use std::{collections::HashMap, fs, path::PathBuf, sync::RwLock};
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::{self, Debug, Formatter},
+    fs,
+    io::ErrorKind,
+    path::PathBuf,
+    sync::{
+        mpsc::{self, Receiver, Sender},
+        RwLock,
+    },
+    thread::{self, JoinHandle},
+};
 
 use glium::{
     glutin::{
@@ -22,6 +33,7 @@ use glium::{
 
 use anyhow::Result;
 use image::GenericImageView;
+use mpsc::TryRecvError;
 use serde::Deserialize;
 
 use crate::{
@@ -101,12 +113,32 @@ void main() {
 
 #[derive(Clone, Deserialize)]
 struct Tile {
-    x: f32,
-    z: f32,
-    size: f32,
+    x: i32,
+    z: i32,
+    size: i32,
     precision: f32,
     terrain: String,
     data: Option<Vec<f32>>,
+}
+
+impl Debug for Tile {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Tile")
+            .field("x", &self.x)
+            .field("z", &self.z)
+            .field("size", &self.size)
+            .field("precision", &self.precision)
+            .field("terrain", &self.terrain)
+            .field(
+                "data.len()",
+                &self
+                    .data
+                    .as_ref()
+                    .map(|data| data.len() as isize)
+                    .unwrap_or(-1),
+            )
+            .finish()
+    }
 }
 
 /// Order: clockwise
@@ -134,9 +166,7 @@ fn generate_indices(vertex_count: u32, rows: u32, cols: u32) -> Vec<u32> {
     indices
 }
 
-type Tiles = Vec<(VertexBuffer<Vertex>, IndexBuffer<u32>)>;
-
-#[derive(Default, Debug)]
+#[derive(Debug)]
 struct Bounds {
     xmin: f32,
     xmax: f32,
@@ -144,8 +174,8 @@ struct Bounds {
     zmax: f32,
 }
 
-impl Bounds {
-    fn new() -> Bounds {
+impl Default for Bounds {
+    fn default() -> Self {
         Bounds {
             xmin: f32::NAN,
             xmax: f32::NAN,
@@ -153,72 +183,283 @@ impl Bounds {
             zmax: f32::NAN,
         }
     }
+}
+
+impl Bounds {
     fn update(&mut self, vertex: &Vertex) {
         self.xmin = self.xmin.min(vertex.x());
         self.xmax = self.xmax.max(vertex.x());
         self.zmin = self.zmin.min(vertex.z());
         self.zmax = self.zmax.max(vertex.z());
     }
+    fn expand(&mut self, other: &Self) {
+        self.xmin = self.xmin.min(other.xmin);
+        self.xmax = self.xmax.max(other.xmax);
+        self.zmin = self.zmin.min(other.zmin);
+        self.zmax = self.zmax.max(other.zmax);
+    }
+    fn for_tile(size: i32, x: i32, z: i32) -> Self {
+        Bounds {
+            xmin: (size * x) as f32,
+            xmax: (size * (x + 1)) as f32,
+            zmin: (size * z) as f32,
+            zmax: (size * (z + 1)) as f32,
+        }
+    }
 }
 
-fn load_tiles(display: &Display) -> Result<(Tiles, Bounds)> {
-    let files = fs::read_dir(DCSVersion::Stable.user_folder()?.join("tiles"))?;
-    let paths: Vec<PathBuf> = files
-        .flat_map(|file| file.map(|result| result.path()))
-        .collect();
-    let paths_len = paths.len().to_string();
-    let empty_tile_data = vec![0.0, 0.0, 0.0, 0.0];
-    let mut vbos = Vec::new();
-    let mut index_cache = HashMap::new();
-    let mut bounds = Bounds::new();
-    for (i, path) in paths.iter().enumerate() {
-        let bytes = fs::read(path)?;
-        let tile: Tile = rmp_serde::from_read_ref(&bytes)?;
-        let tile = match &tile.data {
-            Some(_) => tile,
-            None => Tile {
-                data: Some(empty_tile_data.clone()),
-                precision: tile.size,
-                ..tile
-            },
-        };
+struct GPUTile {
+    vertex_buffer: VertexBuffer<Vertex>,
+    index_buffer: IndexBuffer<u32>,
+    bounds: Bounds,
+}
+
+fn distance_2d(a: (f32, f32), b: (f32, f32)) -> f32 {
+    f32::sqrt(f32::powi(f32::abs(a.0 - b.0), 2) + f32::powi(f32::abs(a.1 - b.1), 2))
+}
+
+fn distance_to(coords: &glm::Vec3, bounds: &Bounds) -> f32 {
+    let (nearest_x, nearest_z) = (
+        if coords.x < bounds.xmin {
+            bounds.xmin
+        } else if coords.x < bounds.xmax {
+            return 0.0;
+        } else {
+            bounds.xmax
+        },
+        if coords.z < bounds.zmin {
+            bounds.zmin
+        } else if coords.z < bounds.zmax {
+            return 0.0;
+        } else {
+            bounds.zmax
+        },
+    );
+    distance_2d((coords.x, coords.z), (nearest_x, nearest_z))
+}
+
+fn tiles_around(
+    coords: &glm::Vec3,
+    range: f32,
+    tile_size: i32,
+) -> impl Iterator<Item = (i32, i32)> {
+    let tile_size = tile_size;
+    let range_tiles = f32::ceil(range / tile_size as f32) as i32;
+    let xcenter = coords.x;
+    let zcenter = coords.z;
+    let xtile = f32::floor(xcenter / tile_size as f32) as i32;
+    let ztile = f32::floor(zcenter / tile_size as f32) as i32;
+    let mut x = xtile - range_tiles;
+    let mut z = ztile - range_tiles;
+    std::iter::from_fn(move || {
+        if x < xtile + range_tiles {
+            if z < ztile + range_tiles {
+                let tile;
+                if distance_to(
+                    &glm::Vec3::new(xcenter, 0.0, zcenter),
+                    &Bounds::for_tile(tile_size, x, z),
+                ) <= range
+                {
+                    tile = Some(Some((x as i32, z as i32)));
+                } else {
+                    tile = Some(None);
+                }
+                z += 1;
+                tile
+            } else {
+                z = ztile - range_tiles;
+                x += 1;
+                Some(None)
+            }
+        } else {
+            None
+        }
+    })
+    .flatten()
+}
+
+struct TileRequest {
+    x: i32,
+    z: i32,
+    size: i32,
+    terrain: String,
+}
+
+struct TileMap {
+    index_cache: HashMap<u32, Vec<u32>>,
+    active_tiles: HashMap<(i32, i32), GPUTile>,
+    queued_tiles: HashSet<(i32, i32)>,
+    total_bounds: Bounds,
+
+    _w: JoinHandle<()>,
+    tx: Sender<TileRequest>,
+    rx: Receiver<Tile>,
+}
+
+impl Default for TileMap {
+    fn default() -> Self {
+        let (tx, thread_rx) = mpsc::channel();
+        let (thread_tx, rx) = mpsc::channel();
+        TileMap {
+            index_cache: Default::default(),
+            active_tiles: Default::default(),
+            queued_tiles: Default::default(),
+            total_bounds: Default::default(),
+            _w: thread::Builder::new()
+                .name(String::from("TileMap worker"))
+                .spawn(move || TileMap::worker_func_unwrapper(thread_rx, thread_tx))
+                .unwrap(),
+            tx,
+            rx,
+        }
+    }
+}
+
+impl TileMap {
+    const EMPTY_TILE: &'static [f32] = &[0.0, 0.0, 0.0, 0.0];
+    const STREAM_RANGE: f32 = 64_000.0;
+    const TILE_SIZE: i32 = 16_000;
+
+    fn worker_func_unwrapper(rx: Receiver<TileRequest>, tx: Sender<Tile>) {
+        TileMap::worker_func(rx, tx).unwrap();
+    }
+
+    fn worker_func(rx: Receiver<TileRequest>, tx: Sender<Tile>) -> Result<()> {
+        loop {
+            let request = rx.recv()?;
+            let path = PathBuf::from(DCSVersion::Stable.user_folder()?.join("tiles").join(
+                format!("caucasus_{}_{}_{}.pack", request.size, request.x, request.z),
+            ));
+            // Load data from disk
+            let tile: Tile = match fs::read(path) {
+                Ok(bytes) => rmp_serde::from_read_ref(&bytes)?,
+                Err(e) => match e.kind() {
+                    ErrorKind::NotFound => {
+                        println!(
+                            "no data available for ({}, {})",
+                            request.x, request.z
+                        );
+                        continue;
+                    }
+                    _ => Err(e)?,
+                },
+            };
+            // Fill tile with a flat surface if it contains no data
+            let tile = match &tile.data {
+                Some(_) => tile,
+                None => Tile {
+                    data: Some(TileMap::EMPTY_TILE.into()),
+                    precision: tile.size as f32,
+                    ..tile
+                },
+            };
+            println!("loaded tile ({}, {}) from disk", tile.x, tile.z);
+            tx.send(tile)?;
+        }
+    }
+
+    fn create_gpu_tile(
+        &mut self,
+        tile: &Tile,
+        display: &Display,
+        x: i32,
+        z: i32,
+    ) -> Result<GPUTile> {
+        print!("processing tile ({}, {})... ", x, z);
         let heights = tile.data.as_ref().unwrap();
-        print!(
-            "{:>width$}/{} Processing tile ({}, {})... ",
-            i,
-            paths_len,
-            tile.x,
-            tile.z,
-            width = paths_len.len()
-        );
-        let rows = (tile.size / tile.precision) as u32 + 1;
+        let rows = (tile.size as f32 / tile.precision) as i32 + 1;
         let cols = rows;
         let positions: Vec<_> = heights
             .iter()
             .enumerate()
             .map(|(i, &y)| {
-                let i = i as u32;
+                let i = i as i32;
                 Vertex::new(
-                    tile.x * tile.size + (i / cols) as f32 * tile.precision,
+                    (tile.x * tile.size) as f32 + (i / cols) as f32 * tile.precision,
                     y,
-                    tile.z * tile.size + (i % cols) as f32 * tile.precision,
+                    (tile.z * tile.size) as f32 + (i % cols) as f32 * tile.precision,
                 )
             })
             .collect();
-        let indices = index_cache
-            .entry(heights.len())
-            .or_insert_with_key(|&vertex_count| generate_indices(vertex_count as u32, rows, cols));
+
+        let indices = self
+            .index_cache
+            .entry(heights.len() as u32)
+            .or_insert_with_key(|&vertex_count| {
+                generate_indices(vertex_count, rows as u32, cols as u32)
+            });
+
+        let mut bounds = Bounds::default();
+        positions.iter().for_each(|pos| bounds.update(pos));
+
+        print!("uploading {} triangles to GPU... ", indices.len() / 3);
         let vbo = VertexBuffer::new(display, &positions)?;
         let ibo = IndexBuffer::new(display, PrimitiveType::TrianglesList, &indices)?;
-        positions.iter().for_each(|pos| bounds.update(pos));
-        vbos.push((vbo, ibo));
         println!("done");
+
+        Ok(GPUTile {
+            vertex_buffer: vbo,
+            index_buffer: ibo,
+            bounds: bounds,
+        })
     }
-    println!(
-        "Finished creating {} triangles",
-        vbos.iter().map(|(_, ibo)| ibo.len() / 3).sum::<usize>()
-    );
-    Ok((vbos, bounds))
+
+    fn update(&mut self, display: &Display, coords: &glm::Vec3) -> Result<()> {
+        let mut updated = false;
+
+        // Queue up tiles in range for loading
+        for (x, z) in tiles_around(coords, Self::STREAM_RANGE, Self::TILE_SIZE) {
+            if !self.active_tiles.contains_key(&(x, z)) && !self.queued_tiles.contains(&(x, z)) {
+                self.queued_tiles.insert((x, z));
+                self.tx.send(TileRequest {
+                    x,
+                    z,
+                    size: Self::TILE_SIZE,
+                    terrain: String::from("caucasus"),
+                })?;
+            }
+        }
+
+        // Add processed tiles from the queue
+        loop {
+            match self.rx.try_recv() {
+                Ok(tile) => {
+                    let new_tile = self.create_gpu_tile(&tile, &display, tile.x, tile.z)?;
+                    self.active_tiles.insert((tile.x, tile.z), new_tile);
+                    self.queued_tiles.remove(&(tile.x, tile.z));
+                    updated = true;
+                }
+                Err(TryRecvError::Empty) => break,
+                Err(e) => Err(e)?,
+            }
+        }
+
+        // Remove tiles out of range
+        self.active_tiles.retain(|&(x, z), tile| {
+            if distance_to(coords, &tile.bounds) > Self::STREAM_RANGE {
+                println!("dropping tile ({}, {})", x, z);
+                updated = true;
+                false
+            } else {
+                true
+            }
+        });
+
+        // Update the bounds
+        if updated {
+            self.total_bounds = Bounds::default();
+            for (_, tile) in &self.active_tiles {
+                self.total_bounds.expand(&tile.bounds);
+            }
+            println!(
+                "total loaded tiles: {}, {:?}",
+                self.active_tiles.len(),
+                self.total_bounds
+            );
+        }
+
+        Ok(())
+    }
 }
 
 /// Makes the window transparent and returns the required viewport and scissor rects for drawing in the window
@@ -263,12 +504,12 @@ unsafe fn make_transparent(display: &Display) -> (Rect, Rect) {
 
 fn draw(
     mut frame: Frame,
-    tiles: &Tiles,
+    tile_map: &TileMap,
     program: &Program,
     view_matrix: &glm::Mat4,
     draw_params: &DrawParameters,
     texture: &Texture2d,
-) {
+) -> Result<()> {
     let uniforms = uniform! {
         view_matrix: [
             [ view_matrix[(0, 0)], view_matrix[(1, 0)], view_matrix[(2, 0)], view_matrix[(3, 0)] ],
@@ -281,12 +522,16 @@ fn draw(
             .anisotropy(8),
     };
     frame.clear_color_and_depth((0.0, 0.0, 0.0, 0.0), 1.0);
-    for tile in tiles {
-        frame
-            .draw(&tile.0, &tile.1, &program, &uniforms, draw_params)
-            .unwrap();
+    for (_, tile) in &tile_map.active_tiles {
+        frame.draw(
+            &tile.vertex_buffer,
+            &tile.index_buffer,
+            &program,
+            &uniforms,
+            draw_params,
+        )?
     }
-    frame.finish().unwrap();
+    Ok(frame.finish()?)
 }
 
 pub fn create(data_handle: &RwLock<Option<FlightData>>) {
@@ -299,9 +544,6 @@ pub fn create(data_handle: &RwLock<Option<FlightData>>) {
     let mut display = Display::new(window, context, &event_loop).unwrap();
     let (viewport, scissor) = unsafe { make_transparent(&mut display) };
     let program = Program::from_source(&display, VS, PS, None).unwrap();
-
-    let (tiles, bounds) = load_tiles(&display).unwrap();
-    println!("{:#?}", bounds);
 
     let draw_params = DrawParameters {
         viewport: Some(viewport),
@@ -323,6 +565,8 @@ pub fn create(data_handle: &RwLock<Option<FlightData>>) {
         let image = RawImage2d::from_raw_rgba_reversed(&image.into_rgba8(), dimensions);
         Texture2d::new(&display, image).unwrap()
     };
+
+    let mut tile_map = TileMap::default();
 
     // Hack the data reference lifetime away (unsound!)
     let data_handle: &'static RwLock<Option<FlightData>> = unsafe { &*(data_handle as *const _) };
@@ -367,20 +611,23 @@ pub fn create(data_handle: &RwLock<Option<FlightData>>) {
                 },
                 ..FlightData::default()
             });
-            let view_matrix = glm::perspective(16.0 / 9.0, f32::to_radians(50.0), 0.5, 100000.0)
-                * glm::look_at_rh(
-                    &fd.cam.p.as_glm_vec3(),
-                    &(fd.cam.p.as_glm_vec3() + fd.cam.x.as_glm_vec3() * 100.0),
-                    &fd.cam.y.as_glm_vec3(),
-                );
+            let cam_pos = fd.cam.p.as_glm_vec3();
+            let cam_fwd = fd.cam.x.as_glm_vec3();
+            let cam_up = fd.cam.y.as_glm_vec3();
+            let view_matrix = glm::perspective(16.0 / 9.0, f32::to_radians(50.0), 0.5, 50_000.0)
+                * glm::look_at_rh(&cam_pos, &(cam_pos + cam_fwd * 100.0), &cam_up);
+
+            tile_map.update(&display, &cam_pos).unwrap();
+
             draw(
                 display.draw(),
-                &tiles,
+                &tile_map,
                 &program,
                 &view_matrix,
                 &draw_params,
                 &texture,
-            );
+            )
+            .unwrap();
         }
         _ => (),
     });
